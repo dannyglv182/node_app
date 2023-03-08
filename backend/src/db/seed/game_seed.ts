634e83c4914db6b151570bd662b40ceebcb9f736
Danny <dannyglv182@gmail.com>
2023-03-08 11:17:07 -0800

    Added seeding file for games.

---
+++ backend/src/db/seed/game_seed.ts
@@ -0,0 +1,16 @@
+/*
+ * Data obtained from https://www.kaggle.com/datasets/gregorut/videogamesales
+ * https://github.com/GregorUT/vgchartzScrape
+*/
+let games: string[] = [
+    "Super Mario World	SNES	1990	Platform	Nintendo	12.78",
+    "Super Mario All-Stars	SNES	1993	Platform	Nintendo	5.99",
+    "Donkey Kong Country	SNES	1994	Platform	Nintendo	4.36",
+    "Super Mario Kart	SNES	1992	Racing	Nintendo	3.54",
+    "Street Fighter II: The World Warrior	SNES	1992	Fighting	Capcom	2.47",
+    "Donkey Kong Country 2: Diddy's Kong Quest	SNES	1995	Platform	Nintendo	2.1",
+    "The Legend of Zelda: A Link to the Past	SNES	1991	Action	Nintendo	2.42",
+    "Super Mario World 2: Yoshi's Island	SNES	1995	Platform	Nintendo	1.65",
+    "Street Fighter II Turbo	SNES	1992	Fighting	Capcom	1.42",
+    "Donkey Kong Country 3: Dixie Kong's Double Trouble!	SNES	1996	Platform	Nintendo	1.17"
+]
