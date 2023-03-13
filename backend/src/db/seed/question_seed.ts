dfcea73d2e6bf5a0bd1c53527318a50269b6d195
Danny <dannyglv182@gmail.com>
2023-03-13 10:23:55 -0700

    questions get assigned to users during seeding now.

--- backend/src/db/seed/question_seed.ts
+++ backend/src/db/seed/question_seed.ts
@@ -2,12 +2,15 @@ import {FastifyInstance} from "fastify";
 import { Question } from "../models/user.js";
 import { AppDataSource } from "../datasources/dev_ds.js";
 import { Game } from "../models/user.js";
+import { User } from "../models/user.js";
 
 export async function QuestionSeeder(app: FastifyInstance){ 
 
     const gameRepository = AppDataSource.getRepository(Game);
     const questionRepository = AppDataSource.getRepository(Question);
+    const userRepository = AppDataSource.getRepository(User);
     const games = await gameRepository.find();
+    const users = await userRepository.find();
     let questions: string[] = [
         "Is there anywhere I can get a bunch of extra lives?",
         "How do I get the last key?",
@@ -36,7 +39,7 @@ export async function QuestionSeeder(app: FastifyInstance){
            take: 1,
        })
 
-       // Seed the question table if it's empty
+       // Seed the question table if it's empty and assign the questions to games
        if (qsInDb.length < 1) {
         for (let i = 0; i < questions.length; i++) {
             let newQuestion = new Question();
@@ -45,9 +48,11 @@ export async function QuestionSeeder(app: FastifyInstance){
 
             if (i == 0) {
                 newQuestion.owningGame = games[0];
+                newQuestion.AskingUser = users[0];
             }
             else {
                 newQuestion.owningGame = games[(games.length - 1) % i];
+                newQuestion.AskingUser = users[(users.length - 1) % i];
             }
             await newQuestion.save();
         }
